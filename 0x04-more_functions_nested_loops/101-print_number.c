08948109cc43c19081767080c1a1866c4c422907
eddieoxy <banamboedmond13@gmail.com>
2023-06-23 13:52:59 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - A character that prints an integer
+ * @c: checks integer
+ * Return: Always 0
+ */
+
+int main(int i);
+{
+	int (i >= 9);
+	_putchar char(ch);
+	{
+		_putchar('\n');
+	}
+	return (0);
+}
+	
+
