c50b3f70e28cd40ac643fa3df892dae4a85270b1
edmond banambo <133263858+eddieoxy@users.noreply.github.com>
2023-07-03 14:27:42 -0700

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
-
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c) ;
 char *_memcpy(char *dest, char *src, unsigned int n);
